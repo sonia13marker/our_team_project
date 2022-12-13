93710746392f03b7721cae8bba9566760c00dda4
Grusl <ffhshdxva@gmail.com>
2022-12-13 21:06:47 +0300

    Intermediate result footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,35 @@
+import style from './style.module.css'
+
+import classnames from 'classnames';
+
+
 export const Footer = () => {
-    return (<></>)
+    return (
+        <footer className={style.footer}>
+            <ul className={classnames(style.row)}>
+                <li className={classnames(style.item_box_left, style.item, style.item_box)}>
+                    <a className={style.style_link} href="#">WebClick</a>
+                </li>
+                <li className={classnames(style.item_box_center, style.item, style.item_box)}>
+                    <ul className={style.row}>
+                        <li className={style.item}><a className={style.style_link} href="#">О нас</a></li>
+                        <li className={style.item}><a className={style.style_link} href="#">Портфолио</a></li>
+                        <li className={style.item}><a className={style.style_link} href="#">Контакты</a></li>
+                    </ul>
+                </li>
+                <li className={classnames(style.item_box_right, style.item, style.item_box)}>
+                    <ul className={style.row}>
+                        <li className={style.item}>
+                            <div id="languages" className={style.languages_content}>
+                                <button>Русский</button>
+                                <button>Английский</button>
+                            </div>
+                            <button className={style.languages_btn}>Язык ^</button>
+                        </li>
+                        <li className={style.item}><a className={style.style_link} href="#">Тема</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </footer>
+    )
 }
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -3,16 +3,18 @@ import {Button} from "../../components/Button/Button";
 import {LeftPanel} from "../../components/LeftPanel/LeftPanel";
 
 export const MainPage = () => {
-    return <div className={style.main_page}>
-        <LeftPanel />
-        <div className={style.wrapper}>
-            <div className={style.contentSide}>
-                <div className={style.infoBlock}>
-                    <p><span className={style.headingTitle1}>WebClick - </span><span className={style.headingTitle1Stroke1}>WebClick - </span><br />креативное агенство <br />по созданию веб-сайтов</p>
-                    <Button />
+    return (
+        <div className={style.main_page}>
+            <LeftPanel />
+            <div className={style.wrapper}>
+                <div className={style.contentSide}>
+                    <div className={style.infoBlock}>
+                        <p><span className={style.headingTitle1}>WebClick - </span><span className={style.headingTitle1Stroke1}>WebClick - </span><br />креативное агенство <br />по созданию веб-сайтов</p>
+                        <Button />
+                    </div>
                 </div>
+                <div className={style.image}></div>
             </div>
-            <div className={style.image}></div>
         </div>
-    </div>
+    )
 }
