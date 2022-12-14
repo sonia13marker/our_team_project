084a7e249552a9b04d4084e8ace2323e297183c0
Grusl <ffhshdxva@gmail.com>
2022-12-14 21:34:00 +0300

    Bug fixes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,13 @@
 import style from './style.module.css';
-import lang_arr from '../../images/lang_arr.svg';
 import classnames from 'classnames';
 import logo from '../../images/logo1_white.svg';
-
 
 export const Footer = () => {
     return (
         <footer className={style.footer}>
-            <ul className={classnames(style.row)}>
+            <ul className={style.row}>
                 <li className={classnames(style.item_box_left, style.item, style.item_box)}>
-                    <a className={style.style_link} href="/#"><img src={logo}/></a>
+                    <a className={style.style_link} href="/#"><img src={logo} alt="logo"/></a>
                     {/* <a className={style.style_link} href="/#">WebClick</a> */}
                 </li>
                 <li className={classnames(style.item_box_center, style.item, style.item_box)}>
@@ -22,17 +20,11 @@ export const Footer = () => {
                 <li className={classnames(style.item_box_right, style.item, style.item_box)}>
                     <ul className={style.row}>
                         <li className={style.item}>
-                            <div id="languages" className={style.languages_content}>
-                                <button>Русский</button>
-                                <button>Английский</button>
-                            </div>
-                            <button className={style.languages_btn}>Язык 
-                            <span className={style.lang_arrow}>
-                                <img src={lang_arr} alt=""/>
-                                </span>
-                            </button>
+                            <button className={style.btn}>Язык</button>
                         </li>
-                        <li className={style.item}><a className={style.style_link} href="/#">Тема</a></li>
+                        <li className={style.item}>
+                            <button className={style.btn}>Тема</button>
+                        </li>
                     </ul>
                 </li>
             </ul>
