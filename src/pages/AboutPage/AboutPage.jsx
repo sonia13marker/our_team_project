9d8f94a5588c3d49330da3555c23705d09bd7170
Anton <anton.lobkov.mtb@gmail.com>
2022-12-15 15:39:52 +0300

    исправил большое количество багов, добавил хедер

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,19 +1,16 @@
 import {TechStack} from "../../components/TechStack/TechStack";
 import { SoftSkills } from "../../components/SoftSkills/SoftSkills";
-import { Footer } from "../../components/Footer/Footer";
 import {AboutComp} from '../../components/AboutComp/AboutComp';
 import style from './style.module.css';
 import {OurTeam} from '../../components/OurTeam/OurTeam';
 
 export function AboutPage() {
     return <div className={style.back}>
-        {/* <Header/> */}
+        <h1 className={style.about_header}>О нас</h1>
         <AboutComp/>
         <TechStack/>
         
         <SoftSkills/>
         <OurTeam />
-        <Footer />
-
     </div>
 }
