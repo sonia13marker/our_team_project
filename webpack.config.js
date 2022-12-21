19907bcce32d582dd432ed5c59a0a6acdd34a2fc
sonia13marker <soniamarker13@gmail.com>
2022-12-22 01:57:28 +0300

    настройка
    пробую настроить деплой

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
+  entry: 'index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ],
     module: {
       rules: [
         {
@@ -7,4 +18,45 @@ module.exports = {
         },
       ],
     },
+    mode: 'production',
+    target: 'web',
+
+    devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    open: true
+  },
+
+  module: {
+    rules: [
+        {
+
+        // @TODO js rule
+        test: /\.(js|jsx)$/i,
+        use: { loader: 'babel-loader',
+        options: {
+        presets: [
+          '@babel/preset-env', ['@babel/preset-react', { 
+            runtime: 'automatic' }
+            ]
+            ],
+            },
+            },
+        exclude: /(node_modules)/,
+
+        resolve: { extensions: ['.js', '.jsx'] },
+    },
+    
+    {
+
+        // @TODO css rule
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+       
+
+}, 
+       
+    ],
+},
   };
