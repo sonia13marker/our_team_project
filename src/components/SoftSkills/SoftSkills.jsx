f3d96e697e684436954a11552e94e5381b880c8a
NailGilmanov <NailGIlmanov27@mail.ru>
2022-12-21 14:59:39 +0300

    Анимация softskills

--- src/components/SoftSkills/SoftSkills.jsx
+++ src/components/SoftSkills/SoftSkills.jsx
@@ -1,6 +1,8 @@
 import style from './style.module.css'
 import React, {Component} from 'react';
 
+import "./style.css";
+
 import { withTranslation } from "react-i18next";
 
 class SoftSkills extends Component {
@@ -50,39 +52,39 @@ class SoftSkills extends Component {
                 </div>
                 <div className={style.content}>
                     <ul className={style.ul}>
-                        <li className={style.item} onMouseEnter={() => this.handleClick(this.text1, this.arrowTip1, this.arrow1, "style_active__OB3RR", "style_animated_arrow_tip__u-D1S", "style_animated_arrow__CirQM")} onMouseLeave={() => this.handleClick(this.text1, this.arrowTip1, this.arrow1, "style_active__OB3RR", "style_animated_arrow_tip__u-D1S", "style_animated_arrow__CirQM")}>
+                        <li className={style.item} onMouseEnter={() => this.handleClick(this.text1, this.arrowTip1, this.arrow1, "active", "animated_arrow_tip", "animated_arrow")} onMouseLeave={() => this.handleClick(this.text1, this.arrowTip1, this.arrow1, "active", "animated_arrow_tip", "animated_arrow")}>
                             <div ref={this.text1}>{t("responsibility")}</div>
                             <div ref={this.arrowTip1}></div>
                             <div ref={this.arrow1}></div>
                         </li>
-                        <li className={style.item} onMouseEnter={() => this.handleClick(this.text2, this.arrowTip2, this.arrow2, "style_active__OB3RR", "style_animated_arrow_tip__u-D1S", "style_animated_arrow__CirQM")} onMouseLeave={() => this.handleClick(this.text2, this.arrowTip2, this.arrow2, "style_active__OB3RR", "style_animated_arrow_tip__u-D1S", "style_animated_arrow__CirQM")}>
+                        <li className={style.item} onMouseEnter={() => this.handleClick(this.text2, this.arrowTip2, this.arrow2, "active", "animated_arrow_tip", "animated_arrow")} onMouseLeave={() => this.handleClick(this.text2, this.arrowTip2, this.arrow2, "active", "animated_arrow_tip", "animated_arrow")}>
                             <div ref={this.text2}>{t("quality")}</div>
                             <div ref={this.arrowTip2}></div>
                             <div ref={this.arrow2}></div>
                         </li>
-                        <li className={style.item} onMouseEnter={() => this.handleClick(this.text3, this.arrowTip3, this.arrow3, "style_active__OB3RR", "style_animated_arrow_tip__u-D1S", "style_animated_arrow__CirQM")} onMouseLeave={() => this.handleClick(this.text3, this.arrowTip3, this.arrow3, "style_active__OB3RR", "style_animated_arrow_tip__u-D1S", "style_animated_arrow__CirQM")}>
+                        <li className={style.item} onMouseEnter={() => this.handleClick(this.text3, this.arrowTip3, this.arrow3, "active", "animated_arrow_tip", "animated_arrow")} onMouseLeave={() => this.handleClick(this.text3, this.arrowTip3, this.arrow3, "active", "animated_arrow_tip", "animated_arrow")}>
                             <div ref={this.text3}>{t("initiative")}</div>
                             <div ref={this.arrowTip3}></div>
                             <div ref={this.arrow3}></div>
                         </li>
-                        <li className={style.item} onMouseEnter={() => this.handleClick(this.text4, this.arrowTip4, this.arrow4, "style_active__OB3RR", "style_animated_arrow_tip__u-D1S", "style_animated_arrow__CirQM")} onMouseLeave={() => this.handleClick(this.text4, this.arrowTip4, this.arrow4, "style_active__OB3RR", "style_animated_arrow_tip__u-D1S", "style_animated_arrow__CirQM")}>
+                        <li className={style.item} onMouseEnter={() => this.handleClick(this.text4, this.arrowTip4, this.arrow4, "active", "animated_arrow_tip", "animated_arrow")} onMouseLeave={() => this.handleClick(this.text4, this.arrowTip4, this.arrow4, "active", "animated_arrow_tip", "animated_arrow")}>
                             <div ref={this.text4}>{t("cohesion")}</div>
                             <div ref={this.arrowTip4}></div>
                             <div ref={this.arrow4}></div>
                             <div>
                             </div>
                         </li>
-                        <li className={style.item} onMouseEnter={() => this.handleClick(this.text5, this.arrowTip5, this.arrow5, "style_active__OB3RR", "style_animated_arrow_tip__u-D1S", "style_animated_arrow__CirQM")} onMouseLeave={() => this.handleClick(this.text5, this.arrowTip5, this.arrow5, "style_active__OB3RR", "style_animated_arrow_tip__u-D1S", "style_animated_arrow__CirQM")}>
+                        <li className={style.item} onMouseEnter={() => this.handleClick(this.text5, this.arrowTip5, this.arrow5, "active", "animated_arrow_tip", "animated_arrow")} onMouseLeave={() => this.handleClick(this.text5, this.arrowTip5, this.arrow5, "active", "animated_arrow_tip", "animated_arrow")}>
                             <div ref={this.text5}>{t("sociability")}</div>
                             <div ref={this.arrowTip5}></div>
                             <div ref={this.arrow5}></div>
                         </li>
-                        <li className={style.item} onMouseEnter={() => this.handleClick(this.text6, this.arrowTip6, this.arrow6, "style_active__OB3RR", "style_animated_arrow_tip__u-D1S", "style_animated_arrow__CirQM")} onMouseLeave={() => this.handleClick(this.text6, this.arrowTip6, this.arrow6, "style_active__OB3RR", "style_animated_arrow_tip__u-D1S", "style_animated_arrow__CirQM")}>
+                        <li className={style.item} onMouseEnter={() => this.handleClick(this.text6, this.arrowTip6, this.arrow6, "active", "animated_arrow_tip", "animated_arrow")} onMouseLeave={() => this.handleClick(this.text6, this.arrowTip6, this.arrow6, "active", "animated_arrow_tip", "animated_arrow")}>
                             <div ref={this.text6}>{t("completion_on_time")}</div>
                             <div ref={this.arrowTip6}></div>
                             <div ref={this.arrow6}></div>
                         </li>
-                        <li className={style.item} onMouseEnter={() => this.handleClick(this.text7, this.arrowTip7, this.arrow7, "style_active__OB3RR", "style_animated_arrow_tip__u-D1S", "style_animated_arrow__CirQM")} onMouseLeave={() => this.handleClick(this.text7, this.arrowTip7, this.arrow7, "style_active__OB3RR", "style_animated_arrow_tip__u-D1S", "style_animated_arrow__CirQM")}>
+                        <li className={style.item} onMouseEnter={() => this.handleClick(this.text7, this.arrowTip7, this.arrow7, "active", "animated_arrow_tip", "animated_arrow")} onMouseLeave={() => this.handleClick(this.text7, this.arrowTip7, this.arrow7, "active", "animated_arrow_tip", "animated_arrow")}>
                             <div ref={this.text7}>{t("dedication_to_work")}</div>
                             <div ref={this.arrowTip7}></div>
                             <div ref={this.arrow7}></div>
