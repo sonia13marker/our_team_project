ef8c9e0165bff9d27d13a09da5616f4211021e98
sonia13marker <soniamarker13@gmail.com>
2022-12-22 17:03:58 +0300

    удаление ненужного

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -19,14 +19,14 @@ export const FormInput = () => {
             <input type="hidden" name="access_key" value="69d63dec-108c-4a88-b54c-2732247f75bb"/>
             <label className={style.label}>
                 {t("name_surname")} <span className={style.red}>*</span><br/>
-                <span><input className={style.input_1} placeholder={t("placeholder1")} {...register("firstName", {
+                <input className={style.input_1} placeholder={t("placeholder1")} {...register("firstName", {
                     required: t("required1"),
                     minLength: {
                         value: 2,
                         message: t("message1")
                     },
                 })}
-                /></span>
+                />
             </label>
             <span className={style.errors}>
             {errors?.firstName && <p>{errors?.firstName?.message || t("error")}</p>}
@@ -36,14 +36,14 @@ export const FormInput = () => {
 
                 <label className={style.label}>
                     E-mail: <span className={style.red}>*</span> <br/>
-                    <span><input className={style.input_2} type="email" placeholder="hello123@mail.ru" {...register("eMail", {
+                    <input className={style.input_2} type="email" placeholder="hello123@mail.ru" {...register("eMail", {
                         required: t("required1"),
 
                         //доделать!
                         pattern: "@",
                         message: t("message2"),
                     })}
-                    /></span>
+                    />
                 </label>
                 <div className={style.errors}>
                     {errors?.eMail && <p>{errors?.eMail?.message || t("error")}</p>}
@@ -52,7 +52,7 @@ export const FormInput = () => {
 
                 <label className={style.label}>
                     {t("phone_number")} <span className={style.red}>*</span><br/>
-                    <span><input className={style.input_2} type="tel"
+                    <input className={style.input_2} type="tel"
                            placeholder="+7 (***) *** - ** - **" {...register("phoneNumber", {
                         required: t("required1"),
                         max: 22,
@@ -62,7 +62,7 @@ export const FormInput = () => {
                         },
 
                     })}
-                    /></span>
+                    />
                 </label>
                 <div className={style.errors}>
                     {errors?.phoneNumber && <p>{errors?.phoneNumber?.message || t("error")}</p>}
@@ -71,13 +71,13 @@ export const FormInput = () => {
 
             <label className={style.label}>
                 {t("message_message")} <br/>
-                <span><input className={style.input_1} type="text" placeholder={t("placeholder2")} {...register("messages", {
+                <input className={style.input_1} type="text" placeholder={t("placeholder2")} {...register("messages", {
                     maxLength: {
                         value: 300,
                         message: t("message4")
                     },
                 })}
-                /></span>
+                />
             </label>
             <div className={style.errors}>
                 {errors?.messages && <p>{errors?.messages?.message || t("error")}</p>}
