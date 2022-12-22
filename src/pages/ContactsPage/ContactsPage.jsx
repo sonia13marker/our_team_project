de3dba5b33ffeaad31d75b8725cd80fe2645ad6a
sonia13marker <soniamarker13@gmail.com>
2022-12-22 16:47:34 +0300

    upd word en

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -4,10 +4,12 @@ import { FormBlock } from '../../components/FormBlock/FormBlock';
 import { Footer } from '../../components/Footer/Footer';
 import { MenuHamburger } from '../../components/MenuHamburger/MenuHamburger';
 
+import { withTranslation } from "react-i18next";
+
 export function ContactsPage() {
     return (<>
             <MenuHamburger/>
-            <h2 className={style.h2}>Контакты</h2>
+            <h2 className={style.h2}>{t("contacts")}</h2>
             <div className={style.wrapper}>
                 <Map />
                 <FormBlock />
